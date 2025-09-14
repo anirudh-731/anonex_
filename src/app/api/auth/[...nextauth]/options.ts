1430779d798325b73fd0d7931ac3def3034f21b3
anirudh-731 <ani.m.rao@gmail.com>
2025-09-14 22:17:50 +0530

    update z03

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -7,8 +7,8 @@ import UserModel from '@/app/model/User';
 export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
-      id: 'credentials',
-      name: 'Credentials',
+      id: 'credentials',   
+      name: 'Credentials',  
       credentials: {
         email: { label: 'Email', type: 'text' },
         password: { label: 'Password', type: 'password' },
--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -7,7 +7,7 @@ export async function middleware(request: NextRequest){
     const token = await getToken({req:request})
     const url = request.nextUrl
 
-    if(token && 
+    if( token && 
         (
             url.pathname.startsWith('/sign-in') ||
             url.pathname.startsWith('/sign-up') ||
@@ -16,6 +16,10 @@ export async function middleware(request: NextRequest){
         )
         ){
         return NextResponse.redirect(new URL('/dashboard', request.url))
+    }
+
+    if(!token && url.pathname.startsWith('/dashboard')){
+        return NextResponse.redirect(new URL ('/sign-in', request.url))
     }
 
     return NextResponse.redirect(new URL('/home', request.url))
@@ -29,4 +33,4 @@ export const config = {
         '/dashboard/:path*',
         '/verify/:path*'
     ]
-}
+}        
